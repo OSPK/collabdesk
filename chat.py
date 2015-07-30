baae4c653598e9e6f92dad0437d6883a0bef8827
OSPK <waqas@opensource.com.pk>
2015-07-31 01:13:27 +0500

    redis

--- chat.py
+++ chat.py
@@ -87,8 +87,8 @@ def stream():
 def home():
 	if 'user' not in flask.session:
 		return flask.redirect('/login')
-	pubsub = red.pubsub()
-	numsub = pubsub.subscription_count
+	#pubsub = red.pubsub()
+	#numsub = pubsub.subscription_count
 	msgs = Msg.query.order_by(Msg.time.desc()).all()
 	user = flask.session['user']
 
