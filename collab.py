e730ee27e034fc2220948dee4b082b9323892022
OSPK <waqas@opensource.com.pk>
2015-08-01 21:32:06 +0500

    iframe source

--- collab.py
+++ collab.py
@@ -193,14 +193,14 @@ def done_count():
 	return b
 
 def home_url():
-	home = str(url_for('index'))
-	url = urlparse(home)
-	home_url = url.hostname
+	home = url_for('index')
+	#url = urlparse(home)
+	#home_url = url.hostname
 
 	if home_url is None:
-		home_url = 'http://localhost'
+		home = 'http://localhost'
 	else:
-		home_url = url.hostname  # will display '127.0.0.1'
+		home = home  # will display '127.0.0.1'
 
 	return home_url
 
