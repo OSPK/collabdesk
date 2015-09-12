f92d3c66c95b4b1af00b390054c5c967c389560c
OSPK <waqas@opensource.com.pk>
2015-09-12 12:39:21 +0500

    sep urdu eng redis keys

--- chat.py
+++ chat.py
@@ -60,8 +60,8 @@ app.config['ONLINE_LAST_MINUTES'] = 5
 def mark_online(user_id):
 	now = int(time.time())
 	expires = now + (app.config['ONLINE_LAST_MINUTES'] * 60) + 10
-	all_users_key = 'online-users/%d' % (now // 60)
-	user_key = 'user-activity/%s' % user_id
+	all_users_key = 'urdu-online-users/%d' % (now // 60)
+	user_key = 'urdu-user-activity/%s' % user_id
 	p = red.pipeline()
 	p.sadd(all_users_key, user_id)
 	p.set(user_key, now)
@@ -70,7 +70,7 @@ def mark_online(user_id):
 	p.execute()
 
 def get_user_last_activity(user_id):
-	last_active = red.get('user-activity/%s' % user_id)
+	last_active = red.get('urdu-user-activity/%s' % user_id)
 	if last_active is None:
 		return None
 	return datetime.utcfromtimestamp(int(last_active))
@@ -78,7 +78,7 @@ def get_user_last_activity(user_id):
 def get_online_users():
 	current = int(time.time()) // 60
 	minutes = xrange(app.config['ONLINE_LAST_MINUTES'])
-	return red.sunion(['online-users/%d' % (current - x) for x in minutes])
+	return red.sunion(['urdu-online-users/%d' % (current - x) for x in minutes])
 
 @app.before_request
 def mark_current_user_online():
